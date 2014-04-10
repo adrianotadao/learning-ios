b65c1a3b68bcf339b180114f5ad981837ac02390
Adriano Tadao <drianotadao@gmail.com>
2014-04-10 00:39:55 -0300

    Setei o storyboard para ser a view principal

---
+++ BlogReader2/BlogReader2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader2
+//
+//  Created by Adriano Tadao on 4/10/14.
+//  Copyright (c) 2014 Adriano Tadao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BlogReader2/BlogReader2Tests/BlogReader2Tests.m
@@ -0,0 +1,34 @@
+//
+//  BlogReader2Tests.m
+//  BlogReader2Tests
+//
+//  Created by Adriano Tadao on 4/10/14.
+//  Copyright (c) 2014 Adriano Tadao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlogReader2Tests : XCTestCase
+
+@end
+
+@implementation BlogReader2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
