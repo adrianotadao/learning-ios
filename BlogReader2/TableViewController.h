c7c829bcf0b50d590fdd04b66515219952961cff
Adriano Tadao <drianotadao@gmail.com>
2014-04-10 00:54:33 -0300

    Fiz popular o table view

---
+++ BlogReader2/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  BlogReader2
+//
+//  Created by Adriano Tadao on 4/10/14.
+//  Copyright (c) 2014 Adriano Tadao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *titles;
+
+@end
